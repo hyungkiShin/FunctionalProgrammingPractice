5ef4ca725b4d249f845fb38de430a75616386785
hyungkiShin <tkaqkeldk@nate.com>
2022-05-05 23:28:25 +0900

    initial Commit

---
+++ src/main/java/com/example/streampt/StreamPtApplication.java
@@ -0,0 +1,13 @@
+package com.example.streampt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamPtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamPtApplication.class, args);
+    }
+
+}
